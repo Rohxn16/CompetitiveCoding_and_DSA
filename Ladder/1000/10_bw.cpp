f0fdc0d8b660e51a24e5f7206c8b851b2fd151e4
Rohxn16 <deyrohan16305@gmail.com>
2024-02-26 22:06:43 +0530

    regular update

--- Ladder/1000/10_bw.cpp
+++ Ladder/1000/10_bw.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int check(string s, int n, int k){
+
+	int ans = n;
+	for(int i = 0; i <= n-k; i++){
+		int b = 0;
+		int w = 0;
+		string sub = "";
+		for(int j = i; j < i+k; j++){
+
+			if(s[j] == 'B')b++;
+			else w++;
+			sub += s[j];
+		}
+		if(b == k)return 0;
+		else ans = min(ans,w);
+
+		// cout << sub << " " <<w <<" "<<b <<endl;
+	}
+	return ans;
+}
+
 int main(int argc, char const *argv[])
 {
 	int t;
@@ -11,8 +32,8 @@ int main(int argc, char const *argv[])
 		string s;
 		cin >> s;
 
-		
-		
+		int ans = check(s,n,k);
+		cout << ans <<endl;
 	}
 	return 0;
 }
